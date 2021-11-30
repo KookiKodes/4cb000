b920055cc2197679e2beb08045271a80caa2e3bc
KookiKodes <kookikodes@gmail.com>
2021-11-30 14:54:38 -0800

    implemented message sort

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -2,13 +2,31 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
+import { sort, pipeInto } from "../../helpers";
+
+const sortMessagesBy = (sortBy) => {
+  switch (sortBy) {
+    case "createdAt":
+      return sort((a, b) => {
+        const x = moment(a.createdAt).valueOf(),
+          y = moment(b.createdAt).valueOf();
+        return x - y;
+      });
+    default:
+      return (arr) => arr;
+  }
+};
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId, sortBy } = props;
+  const sortedMessages = React.useMemo(
+    () => pipeInto(messages, sortMessagesBy(sortBy)),
+    [messages, sortBy]
+  );
 
   return (
     <Box>
-      {messages.map((message) => {
+      {sortedMessages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
