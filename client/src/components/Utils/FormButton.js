12137ace527fd3cf26423e11d71ce785c93cc010
KookiKodes <kookikodes@gmail.com>
2021-12-05 14:38:13 -0800

    Created util button for styling all buttons contained with login and signup pages

---
+++ client/src/components/Utils/FormButton.js
@@ -0,0 +1,9 @@
+import { withStyles, Button } from "@material-ui/core";
+
+export default withStyles({
+  root: {
+    padding: "1rem 1.5rem",
+    fontSize: "1rem",
+    minWidth: "160px",
+  },
+})(Button);
--- client/src/components/Utils/index.js
+++ client/src/components/Utils/index.js
@@ -1,2 +1,3 @@
 export { default as WhiteTypography } from "./WhiteTypography";
 export { default as PageContainer } from "./PageContainer";
+export { default as FormButton } from "./FormButton";
