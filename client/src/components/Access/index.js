c8b869bfcae35ee302e512fe04773abdecef87be
KookiKodes <kookikodes@gmail.com>
2021-12-09 16:10:29 -0800

    created to adhere to import export design

--- client/src/components/Access/index.js
+++ client/src/components/Access/index.js
@@ -1,3 +1,5 @@
 export { default as AccessLayout } from "./Layout";
-export { default as SideBanner } from "./SideBanner";
-export { default as PageContainer } from "./PageContainer";
+export { default as AccessForm } from "./AccessForm";
+export { default as AccessField } from "./AccessField";
+export { default as AccessButton } from "./AccessButton";
+export { default as AccessFieldGroup } from "./AccessFieldGroup";
