ae8be625925e7074042905cf72cb20cc32d03e0d
KookiKodes <kookikodes@gmail.com>
2021-12-09 16:01:07 -0800

    created Layout component

--- client/src/components/Access/Layout.js
+++ client/src/components/Access/Layout.js
@@ -1,58 +1,90 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-  Grid,
   Typography,
   makeStyles,
   Link as MuiLink,
   Box,
 } from "@material-ui/core";
-import { PageContainer, SideBanner } from ".";
+import { PageContainer, SideBanner } from "./utils";
+import { theme } from "../../themes/theme";
 
 const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "3rem",
+    padding: "1.875rem 2.625rem",
+    position: "relative",
+    right: 0,
+    width: "calc(60% - 5.25rem)",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      gap: 0,
+      justifyContent: "center",
+    },
+  },
   link: {
     display: "flex",
     justifyContent: "center",
-    padding: "1rem 2rem",
+    padding: "1rem 2rem 1rem .5rem",
     fontWeight: "600",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: ".75rem",
+    },
+  },
+  flavorText: {
+    fontWeight: "normal",
+    fontSize: ".875rem",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: ".625rem",
+    },
+  },
+  flavorContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+    gap: "2rem",
+    background: "#FFF",
+    [theme.breakpoints.down("sm")]: {
+      borderTopLeftRadius: ".5rem",
+      borderTopRightRadius: ".5rem",
+    },
+  },
+  childrenContainer: {
+    display: "flex",
+    flexDirection: "column",
+    background: "#FFF",
+    height: "70%",
+    paddingLeft: "3rem",
+    paddingRight: "5rem",
+    gap: "1.5rem",
+    [theme.breakpoints.down("sm")]: {
+      borderBottomLeftRadius: ".5rem",
+      borderBottomRightRadius: ".5rem",
+      height: "min-content",
+      padding: "1rem 3rem",
+    },
   },
 });
 
 const Layout = (props) => {
   const classes = useStyles();
   return (
-    <PageContainer container justifyContent="center">
+    <PageContainer>
       <SideBanner />
-      <Grid
-        component={Box}
-        container
-        xs={7}
-        justifyContent="center"
-        alignItems="flex-start"
-        py={3.75}
-        px={5.25}
-      >
-        <Grid
-          container
-          item
-          xs={12}
-          alignItems="center"
-          justifyContent="flex-end"
-          spacing={4}
-        >
-          <Grid item>
-            <Typography variant="body2" color="textSecondary">
-              {props.flavorText}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <MuiLink component={Link} to={props.href} className={classes.link}>
-              {props.linkText}
-            </MuiLink>
-          </Grid>
-        </Grid>
-        <Grid>{props.children}</Grid>
-      </Grid>
+      <Box className={classes.root}>
+        <Box className={classes.flavorContainer}>
+          <Typography className={classes.flavorText} color="textSecondary">
+            {props.flavorText}
+          </Typography>
+          <MuiLink component={Link} to={props.href} className={classes.link}>
+            {props.linkText}
+          </MuiLink>
+        </Box>
+        <Box className={classes.childrenContainer}>{props.children}</Box>
+      </Box>
     </PageContainer>
   );
 };
