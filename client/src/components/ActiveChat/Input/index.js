c71873d988618ff79da24e8ba4302bd11b9da965
KookiKodes <kookikodes@gmail.com>
2021-12-10 13:53:04 -0800

    changed Input component structure

--- client/src/components/ActiveChat/Input/index.js
+++ client/src/components/ActiveChat/Input/index.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import {
+  FormControl,
+  FilledInput,
+  ButtonGroup,
+  Button,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
-import { postMessage } from "../../store/utils/thunkCreators";
+import { postMessage } from "../../../store/utils/thunkCreators";
 
 const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
 }));
 
 const Input = (props) => {
@@ -33,7 +38,7 @@ const Input = (props) => {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
     };
     await postMessage(reqBody);
     setText("");
@@ -49,6 +54,17 @@ const Input = (props) => {
           value={text}
           name="text"
           onChange={handleChange}
+          endAdornment={
+            <ButtonGroup
+              variant="contained"
+              color="primary"
+              aria-label="contained primary button group"
+            >
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          }
         />
       </FormControl>
     </form>
