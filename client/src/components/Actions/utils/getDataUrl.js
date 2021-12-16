e02e78cdf18a0d372a7eb7c12a7b74a496ae4f65
KookiKodes <kookikodes@gmail.com>
2021-12-16 11:05:43 -0800

    Implemented method for resizing an image for cloudinary

--- client/src/components/Actions/utils/getDataUrl.js
+++ client/src/components/Actions/utils/getDataUrl.js
@@ -1,18 +1,58 @@
 const getDataUrl = async (file) => {
+  file = await resize(file);
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () => reject(reader.error);
+    reader.onabort = () => reject(new Error("Read aborted"));
     reader.readAsDataURL(file);
-    reader.addEventListener("load", load);
-    reader.addEventListener("error", error);
-    function load() {
-      resolve(reader.result);
-      reader.removeEventListener("load", load);
-    }
-    function error() {
-      reject(reader.error);
-      reader.removeEventListener("error", error);
-    }
   });
 };
+const MAX_WIDTH = 320;
+const MAX_HEIGHT = 180;
+const MIME_TYPE = "image/jpeg";
+const QUALITY = 0.7;
+
+const resize = async (file) => {
+  return new Promise((resolve, reject) => {
+    const blobURL = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = blobURL;
+    img.onerror = function () {
+      URL.revokeObjectURL(this.src);
+      // Handle the failure properly
+      reject(new Error("Unable to load and resize image"));
+    };
+    img.onload = function () {
+      URL.revokeObjectURL(this.src);
+      const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
+      const canvas = document.createElement("canvas");
+      canvas.width = newWidth;
+      canvas.height = newHeight;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, newWidth, newHeight);
+      canvas.toBlob(resolve, MIME_TYPE, QUALITY);
+    };
+  });
+};
+
+function calculateSize(img, maxWidth, maxHeight) {
+  let width = img.width;
+  let height = img.height;
+
+  // calculate the width and height, constraining the proportions
+  if (width > height) {
+    if (width > maxWidth) {
+      height = Math.round((height * maxWidth) / width);
+      width = maxWidth;
+    }
+  } else {
+    if (height > maxHeight) {
+      width = Math.round((width * maxHeight) / height);
+      height = maxHeight;
+    }
+  }
+  return [width, height];
+}
 
 export default getDataUrl;
