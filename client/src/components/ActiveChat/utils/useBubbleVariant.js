4c64559c71c31796033764ed1a7f0002a5b32203
KookiKodes <kookikodes@gmail.com>
2021-12-22 15:27:38 -0800

    Implemented new bubble to display when other user is typing

--- client/src/components/ActiveChat/utils/useBubbleVariant.js
+++ client/src/components/ActiveChat/utils/useBubbleVariant.js
@@ -1,10 +1,11 @@
 import { useMemo } from "react";
 
-const useBubbleVariant = (text, attachments) => {
+const useBubbleVariant = (text = "", attachments = []) => {
   return useMemo(() => {
     if (text && attachments.length > 1) return "text attachments";
     if (text && attachments.length === 1) return "card";
     if (!text && attachments.length) return "image";
+    if (!text && !attachments.length) return "typing";
     return "default";
   }, [text, attachments]);
 };
